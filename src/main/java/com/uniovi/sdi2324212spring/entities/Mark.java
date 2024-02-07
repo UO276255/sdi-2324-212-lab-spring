dc338d1790a70e5c30fcb3f545bac4e38d32c1b7
ruben <uo276255@uniovi.es>
2024-02-07 19:00:46 +0100

    SDI-2324-212-2.4-Modelo, acceso a datos simple.

--- src/main/java/com/uniovi/sdi2324212spring/entities/Mark.java
+++ src/main/java/com/uniovi/sdi2324212spring/entities/Mark.java
@@ -1,6 +1,13 @@
 package com.uniovi.sdi2324212spring.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+@Entity
+
 public class Mark {
+    @Id
+    @GeneratedValue
     private Long id;
     private String description;
     private Double score;
