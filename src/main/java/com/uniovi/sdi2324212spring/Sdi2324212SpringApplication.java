327c8352883e4b3456227bcd6a3b71fb96bb85fa
ruben <uo276255@uniovi.es>
2024-02-07 18:36:35 +0100

    SDI-2324-212-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi2324212spring/Sdi2324212SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2324212spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2324212SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2324212SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi2324212spring/Sdi2324212SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2324212spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2324212SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
