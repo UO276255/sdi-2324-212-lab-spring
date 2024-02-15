2fb2ff6328f340db6bd931088468e3927d790597
ruben <uo276255@uniovi.es>
2024-02-15 01:31:42 +0100

    SDI-2324-212-3.5-Validación de datos en el servidor.

--- src/main/java/com/uniovi/sdi2324212spring/controllers/UsersController.java
+++ src/main/java/com/uniovi/sdi2324212spring/controllers/UsersController.java
@@ -1,11 +1,14 @@
 package com.uniovi.sdi2324212spring.controllers;
 
 import com.uniovi.sdi2324212spring.services.SecurityService;
+import com.uniovi.sdi2324212spring.validators.SignUpFormValidator;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.uniovi.sdi2324212spring.entities.*;
 import com.uniovi.sdi2324212spring.services.UsersService;
@@ -17,9 +20,12 @@ public class UsersController {
 
     private final UsersService usersService;
     private final SecurityService securityService;
-    public UsersController(UsersService usersService, SecurityService securityService) {
+    private final SignUpFormValidator signUpFormValidator;
+    public UsersController(UsersService usersService, SecurityService securityService, SignUpFormValidator
+            signUpFormValidator) {
         this.usersService = usersService;
         this.securityService = securityService;
+        this.signUpFormValidator = signUpFormValidator;
     }
 
     @PostConstruct
@@ -65,10 +71,20 @@ public class UsersController {
         return "redirect:/user/details/" + id;
     }
 
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signup(@ModelAttribute("user") User user, Model model) {
+    public String signup(@Validated User user, BindingResult result) {
+        signUpFormValidator.validate(user,result);
+        if(result.hasErrors()){
+            return "signup";
+        }
         usersService.addUser(user);
-        securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
+        securityService.autoLogin(user.getDni(),user.getPasswordConfirm());
         return "redirect:home";
     }
 
