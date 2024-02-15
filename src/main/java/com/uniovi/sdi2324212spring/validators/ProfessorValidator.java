eb8da30156b1722d413c914a3516a00fb998fab9
ruben <uo276255@uniovi.es>
2024-02-15 18:56:43 +0100

    SDI-2324-212-sesión4-ejercicio-complementario2

--- src/main/java/com/uniovi/sdi2324212spring/validators/ProfessorValidator.java
+++ src/main/java/com/uniovi/sdi2324212spring/validators/ProfessorValidator.java
@@ -11,14 +11,28 @@ import org.springframework.validation.Validator;
 
 @Component
 public class ProfessorValidator implements Validator {
+    private final ProfessorService professorService;
 
+    public ProfessorValidator(ProfessorService professorService) {
+        this.professorService = professorService;
+    }
     @Override
     public boolean supports(Class<?> clazz) {
-        return false;
+        return Professor.class.equals(clazz);
     }
 
     @Override
     public void validate(Object target, Errors errors) {
-
+        Professor prof = (Professor) target;
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "dni", "Error.empty");
+        if (prof.getDni().length() != 9 ) {
+            errors.rejectValue("dni", "Error.profAdd.dni.length");
+        }
+        if(!Character.isLetter(prof.getDni().charAt(prof.getDni().length()-1))){
+            errors.rejectValue("dni", "Error.profAdd.dni.letter");
+        }
+        if(professorService.getProfessorByDni(prof.getDni()) != null){
+            errors.rejectValue("dni", "Error.signup.dni.duplicate");
+        }
     }
 }
